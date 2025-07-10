7b1b361918286aa4109e4cc86ac874d8826a74d3
Artem Chupylko <achupylko@gmail.com>
2025-07-10 14:05:01 +0000

    Added origins for CORS for frontend and backend interaction.

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, origins=["http://localhost:5500", "http://127.0.0.1:5500"])
 
 @app.route("/api/contact", methods=["POST"])
 def contact():
